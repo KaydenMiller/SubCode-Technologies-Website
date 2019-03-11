c18659f4129681d31a852983b4ac8e84d67a7274
root <root@vps177623.vps.ovh.ca>
2019-03-11 02:03:26 -0400

    Fix spelling error in about us

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
 			Here at SubCode Technologies we develop many different products, some of these being games
 			and others hardware. We are deticated to providing the highest quality products that we can.
 			We were founded by Kayden Miller and Joshua Brown in the year 2015. We desided to make our 
-			company in an effort to make a profit off of what we both enjoy doing.. makeing games. Over
+			company in an effort to make a profit off of what we both enjoy doing.. making games. Over
 			time we have branched into several different fields such as electrical engineering. We hope 
 			that you will enjoy our products and are open to any feedback that you can provide us. As
 			even negative feedback can help to improve things.
